16f2ba3a4531feb15f46193d3cbb98cd1b9f49a5
Danang <walididanang463@gmail.com>
2026-01-09 07:44:16 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const supabaseUrl = 'https://csxcdjlkptanafjdrcvp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzeGNkamxrcHRhbmFmamRyY3ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4OTc5MTEsImV4cCI6MjA4MzQ3MzkxMX0.FYqfmVEINxrBuU9_v1wsF1ZcktYokZ5Qde3Ar_ZPkaI';
+
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
 console.log("Aplikasi Beranda LKS Cloud Dimuat (index.html)");
 console.log("Memulai Proses fetch data untuk fitur dinamis");
 
@@ -20,3 +25,20 @@ fetch(`https://jsonplaceholder.typicode.com/posts/${randomId}`)
     console.error("Terjadi Masalah saat mengambil data API", error);
     document.getElementById('data-api').textContent = 'Gagal memuat data API.';
 })
+
+async function kirimDataKeSupabase() {
+    const { data, error } = await supabase
+        .from('Pesan')
+        .insert([
+            { 
+                nama: 'Walidi Danang', 
+                isi_pesan: 'Tes koneksi database cloud' 
+            }
+        ]);
+
+    if (error) {
+        console.error("Gagal mengirim:", error.message);
+    } else {
+        console.log("Data berhasil masuk ke Cloud!");
+    }
+}
